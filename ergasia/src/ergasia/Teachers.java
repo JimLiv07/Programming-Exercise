f12264cd39f6e881cd3d237131395100ad1aff5d
Jim Leivadis <72867428+JimLiv07@users.noreply.github.com>
2021-01-28 12:37:31 +0200

    Update Teachers.java

--- ergasia/src/ergasia/Teachers.java
+++ ergasia/src/ergasia/Teachers.java
@@ -1,31 +1,25 @@
 package ergasia;
 
 public class Teachers {
-	private static Teachers[] elementaryteachers = new Teachers[6];
-	private static Teachers[] middleteachers = new Teachers[10];
-	private static Teachers[] highteachers = new Teachers[10];
+	private static Teachers[] elementary_teachers = new Teachers[6];
+	private static Teachers[] middle_teachers = new Teachers[10];
+	private static Teachers[] high_teachers = new Teachers[10];
 	private static int c1 = 0;
 	private static int c2 = 0;
 	private static int c3 = 0;
-	private static int tcid; /* 4000-4999*/ /*Ως 4300 δημ , ως 4700 , γυμν ,ως 4999 λυκ*/
+	private static int teachers_id; /* 4000-4999*/ /*Ως 4300 δημ , ως 4700 , γυμν ,ως 4999 λυκ*/
 	private String name;
-	private int classes; /* από 0 έως 5 είναι οι τάξεις του δημοτικού και 6-8 είναι γυμνάσιο και 9-11 λύκειο */
+	private int classes; /* από 0 έως 5 είναι οι τάξεις του δημοτικού */
 	private boolean covid_status;
-	public static int counter_covidel;
-	public static int counter_covidmid;
-	public static int counter_covidhi;
-	public static boolean fe = false;
-	public static boolean fm = false;
-	public static boolean fh = false;
+	public static boolean start = false;
 	
 	public Teachers () {
 	}
 	
-	public Teachers(int data, int type, String name, int cl) {
+	public Teachers(int type, String name, int cl) {
 		if (type == 1)  {
-			if(data >= 4000 && data < 4300) {
-				if (c1 < 6 ) {
-					this.tcid = data;
+			       if (c1 < 6 ) {
+					this.teachers_id = 4000 + c1;
 					this.name = name;
 					this.classes = cl;
 					this.covid_status = false;
@@ -35,13 +29,9 @@ public class Teachers {
 				} else {
 					System.out.println("Δεν υπάρχει άλλη χωρητικότητα για καθηγητές δημοτικού.");
 				}
-			}else {
-			System.out.println("Ο αριθμός μητρώου του καθηγητή δημοτικού πρέπει να είναι από 4000 έως 4299");
-			}
 		}else if (type == 2) {
-			if(data >= 4300 && data < 4700) {
-				if (c2 < 10 ) {
-					this.tcid = data;
+			        if (c2 < 10 ) {
+					this.tcid = 4300 + c2;
 					this.name = name;
 					this.classes = cl;
 					this.covid_status = false;
@@ -51,13 +41,9 @@ public class Teachers {
 				} else {
 					System.out.println("Δεν υπάρχει άλλη χωρητικότητα για καθηγητές γυμνασίου.");
 				}
-			}else {
-			System.out.println("Ο αριθμός μητρώου του καθηγητή γυμνασίου πρέπει να είναι από 4300 έως 4699");
-			}
-		} else if (type == 3){
-			if(data >= 4700 && data < 5000) {
+		} else {
 				if (c3 < 10 ) {
-				this.tcid = data;
+				this.tcid = 4700 + c3;
 				this.name = name;
 				this.classes = cl;
 				this.covid_status = false;
@@ -67,159 +53,126 @@ public class Teachers {
 			    } else {
 				System.out.println("Δεν υπάρχει άλλη χωρητικότητα για καθηγητές λυκείου.");
 			    }
-			} else {
-				System.out.println("Ο αριθμός μητρώου του καθηγητή λυκείου πρέπει να είναι από 4700 έως 4999");
-			}
 	    }	
 		
 	}
-	public void covidChanget(int covid_id) {
-			boolean flag = false;
-			int k = 0;
-			int mode = 0;
-		if (tcid < 4300) {
-			for (int i = 0; i < elementaryteachers.length; i++) {
-				if (elementaryteachers[i] != null) {
-					if (elementaryteachers[i].getTcid() == covid_id) {
-						flag = true;
-						if (elementaryteachers[i].isCovid_status() == false) {
-							mode = 1;
-						} else {
-							mode = 2;
-						}
-						k = i;
-						i = elementaryteachers.length - 1;
-					}
-				}
-			}
-			if (flag == true) {
-				if (mode == 1) {
-					elementaryteachers[k].setCovid_status(true);
-					counter_covidel++;
-				} else {
-					elementaryteachers[k].setCovid_status(false);
-					counter_covidel--;
-				}
-				if(counter_covidel == 2) {
-					System.out.println("Το δημοτικό ξαναξεκίνησε την λειτουργεία του.");
-				}
-			}	
-		} else if (tcid < 4700 ) {
-			for (int i = 0; i < middleteachers.length; i++) {
-				if (middleteachers[i] != null) {
-					if (middleteachers[i].getTcid() == covid_id) {
-						flag = true;
-						if (middleteachers[i].isCovid_status() == false) {
-							mode = 1;
-						} else {
-							mode = 2;
-						}
-						k = i;
-						i = middleteachers.length - 1;
-					}
-				}
-			}
-			if (flag == true) {
-				if (mode == 1) {
-					middleteachers[k].setCovid_status(true);
-					counter_covidel++;
-				} else {
-					middleteachers[k].setCovid_status(false);
-					counter_covidel--;
-				}
-				if(counter_covidel == 0) {
-					System.out.println("Το γυμνάσιο ξαναξεκίνησε την λειτουργεία του.");
-				}
-			}	
-		} else {
-			for (int i = 0; i < highteachers.length; i++) {
-				if (highteachers[i] != null) {
-					if (highteachers[i].getTcid() == covid_id) {
-						flag = true;
-						if (highteachers[i].isCovid_status() == false) {
-							mode = 1;
-						} else {
-							mode = 2;
-						}
-						k = i;
-						i = highteachers.length - 1;
-					}
-				}
-			}
-			if (flag == true) {
-				if (mode == 1) {
-					highteachers[k].setCovid_status(true);
-					counter_covidel++;
-				} else {
-					highteachers[k].setCovid_status(false);
-					counter_covidel--;
-				}
-				if(counter_covidel == 0) {
-					System.out.println("Το λύκειο ξαναξεκίνησε την λειτουργεία του.");
-				}
-			}	
-		}
-			
-	}
-	public void covidStatus() {
-		if(counter_covidel !=0) {
-			if (tcid < 4300) {
-			  for (int i = 0; i < elementaryteachers.length; i++) {
-				if(elementaryteachers[i] != null) {
-					if (elementaryteachers[i].isCovid_status() == true) {
-						if(elementaryteachers[i].getClasses()==0) {
-							System.out.println("\"Α' ΤΑΞΗ ΔΗΜΟΤΙΚΟΥ ΚΛΕΙΣΤΗ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.\"");
-						} else if (elementaryteachers[i].getClasses()==1) {
-							System.out.println("\"B' ΤΑΞΗ ΔΗΜΟΤΙΚΟΥ ΚΛΕΙΣΤΗ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.\"");
-						} else if (elementaryteachers[i].getClasses()==2) {
-							System.out.println("\"Γ' ΤΑΞΗ ΔΗΜΟΤΙΚΟΥ ΚΛΕΙΣΤΗ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.\"");
-						}else if (elementaryteachers[i].getClasses()==3) {
-							System.out.println("\"Δ' ΤΑΞΗ ΔΗΜΟΤΙΚΟΥ ΚΛΕΙΣΤΗ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.\"");
-						}else if (elementaryteachers[i].getClasses()==4) {
-							System.out.println("\"Ε' ΤΑΞΗ ΔΗΜΟΤΙΚΟΥ ΚΛΕΙΣΤΗ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.\"");
-						} else if (elementaryteachers[i].getClasses()==5) {
-							System.out.println("\"ΣΤ' ΤΑΞΗ ΔΗΜΟΤΙΚΟΥ ΚΛΕΙΣΤΗ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.\"");
-						} else {
-							System.out.println("Μέχρι τώρα δεν υπάρχει κάποιο κρούσμα καθηγητή στις τάξεις του δημοτικού.");
-						}	
-					}
-				  }
-			  }
-			} else if (tcid >= 4300 && tcid < 4700) {
-				for (int i = 0; i < middleteachers.length; i++) {
-						if(middleteachers[i] != null) {
-							if (middleteachers[i].isCovid_status() == true) {
-								if (middleteachers[i].getClasses() >= 6 && middleteachers[i].getClasses() <= 8) {
-									System.out.println("\" ΤΟ ΓΥΜΝΑΣΙΟ ΚΛΕΙΣΤΟ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.\"");
-								} else {
-							       System.out.println("Μέχρι τώρα δεν υπάρχει κάποιο κρούσμα καθηγητή στις τάξεις του γυμνασίου.");
-								}
-							}
-						}
-				}
-			} else {
-				for (int i = 0; i < highteachers.length; i++) {
-					if (highteachers[i]!=null) {
-						if (highteachers[i].isCovid_status() == true) {
-							if (highteachers[i].getClasses() <= 9 && highteachers[i].getClasses() >= 11 ) {
-								System.out.println("ΤΟ ΛΥΚΕΙΟ ΚΛΕΙΣΤΟ ΜΕΧΡΙ ΝΕΩΤΕΡΑΣ ΛΟΓΩ ΚΡΟΥΣΜΑΤΟΣ ΕΚΠΑΙΔΕΥΤΙΚΟΥ.");
-							} else {
-								System.out.println("Μέχρι τώρα δεν υπάρχει κάποιο κρούσμα καθηγητή στις τάξεις του λυκείου.");
-						    }
-					   }
-					} 
-				}
-			}
-		}
-	}
-		
 	
+	public void create() {
+		start = true;
+		Teachers teacher1 = new Teachers(1, "Φωτεινή Θεοδωροπούλου", 0);
+		Teachers teacher2 = new Teachers(1, "Πέτρος Αρβανιτάκης", 1);
+		Teachers teacher3 = new Teachers(1, "Ρενάτα Τσαλδάρη", 2);
+		Teachers teacher4 = new Teachers(1, "Φάιη Αντωνοπούλου", 3);
+		Teachers teacher5 = new Teachers(1, "Λίνα Λινοπούλου", 4);
+		Teachers teacher6 = new Teachers(1, "Λίτσα Πατέρα", 5);
+		Teachers teacher7 = new Teachers(2, "Αθηνά Κατσαρού", -1);
+		Teachers teacher8 = new Teachers(2, "Αριστέα Μπουλουξή", -1);
+		Teachers teacher9 = new Teachers(2, "Σοφία Γεωργιάδου", -1);
+		Teachers teacher10 = new Teachers(3, "Μαρία Ντόβα", -1);
+		Teachers teacher11 = new Teachers(3, "Γρηγορία Σωτηροπούλου", -1);
+		Teachers teacher12 = new Teachers(3, "Σπύρος Καρδαμίτσης", -1);
+		start = false;
+	}
+
+	public void covidChanget(int covid_id, int mode) {
+		boolean flag = false;
+		int keep = 0;
+		if (covid_id < 4300) {
+			for (int i = 0; i < elementary_teachers.length; i++) {
+				if (elementary_teachers[i] != null) {
+					if (elementary_teachers[i].getTeachersID() == covid_id) {
+						flag = true;
+						keep = i;
+						i = elementary_teachers.length - 1;
+					}
+				}
+			}
+			if (flag == true) {
+				if (mode == 5) {
+					if (elementary_teachers[keep].getCovidStatus() == true) {
+						System.out.println("Είναι ήδη θετικός ο καθηγητής.");
+					} else {
+						elementary_teachers[keep].setCovidStatus(true);
+						new Elementary().covidChangeTeachers(true, elementary_teachers[keep].getClasses());
+					}
+				} else {
+					if (elementary_teachers[keep].getCovidStatus() == false) {
+						System.out.println("Είναι ήδη αρνητικός ο καθηγητής.");
+					} else {
+						elementary_teachers[keep].setCovidStatus(false);
+						new Elementary().covidChangeTeachers(false, elementary_teachers[keep].getClasses());
+					}
+				}
+			} 
+		} else if (covid_id < 4700 ) {
+			for (int i = 0; i < middle_teachers.length; i++) {
+				if (middle_teachers[i] != null) {
+					if (middle_teachers[i].getTeachersID() == covid_id) {
+						flag = true;
+						keep = i;
+						i = middle_teachers.length - 1;
+					}
+				}
+			}
+			if (flag == true) {
+				if (mode == 5) {
+					if (middle_teachers[keep].getCovidStatus() == true) {
+						System.out.println("Είναι ήδη θετικός ο καθηγητής.");
+					} else {
+						middle_teachers[keep].setCovidStatus(true);
+						new Middle().covidChangeTeachers(true);
+					}
+				} else {
+					if (middle_teachers[keep].getCovidStatus() == false) {
+						System.out.println("Είναι ήδη αρνητικός ο καθηγητής.");
+					} else {
+						middle_teachers[keep].setCovidStatus(false);
+						new Middle().covidChangeTeachers(false);
+					}
+				}
+			}
+		} else {
+			for (int i = 0; i < high_teachers.length; i++) {
+				if (high_teachers[i] != null) {
+					if (high_teachers[i].getTeachersID() == covid_id) {
+						flag = true;
+						keep = i;
+						i = high_teachers.length - 1;
+					}
+				}
+			}
+			if (flag == true) {
+				if (mode == 5) {
+					if (high_teachers[keep].getCovidStatus() == true) {
+						System.out.println("Είναι ήδη θετικός ο καθηγητής.");
+					} else {
+						high_teachers[keep].setCovidStatus(true);
+						new High().covidChangeTeachers(true);
+					}
+				} else {
+					if (high_teachers[keep].getCovidStatus() == false) {
+						System.out.println("Είναι ήδη αρνητικός ο καθηγητής.");
+					} else {
+						System.out.println("Καλησπέρα");
+						high_teachers[keep].setCovidStatus(false);
+						new High().covidChangeTeachers(false);
+					}
+				}
+			}	
+		}
+		if (flag == false) {
+			System.out.println("Δεν υπάρχει καθηγητής με τέτοιο ID.");
+			System.out.println("Οι καθηγητές είναι:");
+			display();
+		}
+	}
+
 	public void display() {
-		int e = 0 ;
-		for(int i = 0 ; i < elementaryteachers.length ; i++ ) {
-			if(elementaryteachers[i] != null) {
-				System.out.println("Ο-Η καθηγητής-τρια της " + (elementaryteachers[i].getClasses() + 1) + " δημοτικού είναι ο-η " + elementaryteachers[i].getName());
-			}else {
+		int e = 0;
+		for(int i = 0; i < elementary_teachers.length; i++) {
+			if(elementary_teachers[i] != null) {
+				System.out.println("Όνομα καθηγητή: " + elementary_teachers[i].getName() + " ID καθηγητή: " + elementary_teachers[i].getTeachersID() + " Τάξεις δημοτικού που διδάσκει: " + (elementary_teachers[i].getClasses() + 1));
+			} else {
 				e++;
 			}
 		}
@@ -227,35 +180,35 @@ public class Teachers {
 			System.out.println("Το δημοτικό δεν έχει πληρότητα καθηγητών");
 		}
 		int m = 0;
-		for(int i = 0 ; i < middleteachers.length ; i++ ) {
-			if(middleteachers[i] == null) {
+		for (int i = 0; i < middle_teachers.length; i++) {
+			if (middle_teachers[i] == null) {
 				m++;
-			}else {
-				System.out.println("Ο-Η καθηγητής-τρια του γυμνασίου είναι ο-η " + middleteachers[i].getName());
+			} else {
+				System.out.println("Όνομα καθηγητή: " + middle_teachers[i].getName() + " ID καθηγητή: " + middle_teachers[i].getTeachersID() + " Τάξεις γυμνασίου που διδάσκει: Σε όλες");
 			}
 		}
-		if(m > 0) {
+		if (m > 0) {
 			System.out.println("Το γυμνάσιο δεν έχει πληρότητα καθηγητών");
 		}
 		int h = 0;
-		for(int i = 0 ; i < highteachers.length ; i++ ) {
-			if(highteachers[i] == null) {
+		for (int i = 0; i < high_teachers.length; i++) {
+			if (high_teachers[i] == null) {
 				h++;
-			}else {
-				System.out.println("Ο-Η καθηγητής-τρια του λυκείου είναι ο-η " + highteachers[i].getName());
+			} else {
+				System.out.println("Όνομα καθηγητή: " + high_teachers[i].getName() + " ID καθηγητή: " + high_teachers[i].getTeachersID() + " Τάξεις λυκείου που διδάσκει: Σε όλες");
 			}
 		}
-		if(h > 0) {
+		if (h > 0) {
 			System.out.println("Το λύκειο δεν έχει πληρότητα καθηγητών");
 		}
 	}
-			
-	public int getTcid() {
-		return tcid;
+
+	public int getTeachersID() {
+		return teachers_id;
 	}
 
-	public void setTcid(int tcid) {
-		this.tcid = tcid;
+	public void setTeachersID(int tcid) {
+		this.teachers_id = tcid;
 	}
 
 	public String getName() {
@@ -266,11 +219,11 @@ public class Teachers {
 		this.name = name;
 	}
 
-	public boolean isCovid_status() {
+	public boolean getCovidStatus() {
 		return covid_status;
 	}
 
-	public void setCovid_status(boolean covid_status) {
+	public void setCovidStatus(boolean covid_status) {
 		this.covid_status = covid_status;
 	}
 
@@ -281,7 +234,5 @@ public class Teachers {
 	public void setClasses(int classes) {
 		this.classes = classes;
 	}
-	
-	
 }
 
